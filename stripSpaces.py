996022e844796c88959810a2ef1850977af1b336
jrom876 <jrom876@gmail.com>
2026-06-01 18:59:01 -0600

    Update stripSpaces.py

--- stripSpaces.py
+++ stripSpaces.py
@@ -20,33 +20,9 @@ def bin_to_dec(binary):
 		i += 1
 	return decimal
 
-# ~ def bin_to_oct(binary):
-	# ~ octal, i = 0, 0
-	# ~ binary = str(binary)
-	# ~ binary = binary.replace(" ", "")
-	# ~ binary = int(binary, 2)
-	# ~ while(binary != 0):
-		# ~ oct = binary % 8
-		# ~ octal = octal + oct * pow(8, i)
-		# ~ binary = binary//8
-		# ~ i += 1
-	# ~ return octal
-
 def bin_to_oct(binary):
 	x = bin_to_dec(binary)
 	return oct(x)
-
-# ~ def bin_to_hex(binary):
-	# ~ hexa, i = 0, 0
-	# ~ binary = str(binary)
-	# ~ binary = binary.replace(" ", "")
-	# ~ binary = int(binary, 2)
-	# ~ while(binary != 0):
-		# ~ hexad = binary % 16
-		# ~ hexa = hexa + hexad * pow(16, i)
-		# ~ binary = binary//16
-		# ~ i += 1
-	# ~ return hex(hexa)
 
 def bin_to_hex(binary):
 	x = bin_to_dec(binary)
@@ -74,10 +50,6 @@ def oct_to_hex(oct):
 	return hex(octa)
 
 #========= Decimal ==========#
-# def dec_to_bin(x):
-#     # return int(bin(x)[2:])
-#     return bin(x)
-
 def dec_to_bin(decimal):
 	binary, i = 0, 0
 	decimal = str(decimal)
@@ -89,10 +61,6 @@ def dec_to_bin(decimal):
 		decimal = decimal//2
 		i += 1
 	return binary
-
-# def dec_to_oct(x):
-#     # return int(oct(x)[2:])
-#     return oct(x)
 
 def dec_to_oct(decimal):
 	octal, i = 0, 0
@@ -119,7 +87,6 @@ def hex_to_bin(hex):
 	dec = dec.replace(" ", "")
 	dec = int(dec, 10)
 	return dec_to_bin(dec)
-
 	
 def hex_to_oct(hex):
 	dec = str(hex)
@@ -136,46 +103,6 @@ def hex_to_dec(hex):
 ########################################
 ### User Interface ###
 def getuserInput():
-	"""Test the hex_to_dec function
-	
-	Usage examples:
-	>>> bin_to_dec("11 11")
-	15
-	
-	>>> bin_to_oct("11 11")
-	'0o17'
-	
-	# ~ >>> bin_to_hex("11 11")	
-	# ~ '0xf'	
-
-	>>> oct_to_bin("2 5")
-	'0b10101'
-	
-	>>> oct_to_dec("2 5")
-	21
-
-	>>> oct_to_hex("2 5")
-	'0x15'
-
-	>>> dec_to_bin("2 5")
-	11001
-	
-	>>> dec_to_oct("18 29")
-	'0o3445'
-
-	>>> dec_to_hex("65 5 34")
-	'0xfffe'
-		
-	>>> hex_to_bin("88 99")
-	'1000100010011001'
-			
-	>>> hex_to_oct("88 99")
-	'0o104231'
-	
-	>>> hex_to_dec("88 99")
-	'34969'
-
-	"""
 	print("1 = Binary to All    2 = Octal to All")
 	print("3 = Decimal to All   4 = Hex to All")
 	print("5 = Exit\n")
@@ -185,7 +112,6 @@ def getuserInput():
 	choice = int(choice)
 
 	if choice == 1:# Binary to all
-		# ~ bin = bin.strip()
 		bin = str(input("Binary in ==> "))
 		bin = bin.replace(" ", "")
 		bin = int(bin)
@@ -197,7 +123,6 @@ def getuserInput():
 		oct = str(input("Octal in ==> "))
 		oct = oct.replace(" ", "")
 		oct = int(oct)
-		# ~ oct = input("Octal in ==> ")
 		print("Binary: ",oct_to_bin(oct))
 		print("Decimal: ",oct_to_dec(oct))
 		print("Hexadecimal: ",oct_to_hex(oct))
@@ -206,7 +131,6 @@ def getuserInput():
 		dec = str(input("Decimal in ==> "))
 		dec = dec.replace(" ", "")
 		dec = int(dec)
-		# ~ dec = int(input("Decimal in ==> "))
 		print("Binary: ",dec_to_bin(dec))
 		print("Octal: ",dec_to_oct(dec))
 		print("Hexadecimal: ",dec_to_hex(dec))
@@ -218,7 +142,6 @@ def getuserInput():
 		print("Binary: ",hex_to_bin(hexa))
 		print("Octal: ",hex_to_oct(hexa))
 		print("Decimal: ",hex_to_dec(hexa))
-		# ~ print("Decimal: ",hex_to_decimal(hexa))
 		return 0
 	elif choice == 5: # Exit
 		exit_flag = 1
@@ -229,9 +152,7 @@ def getuserInput():
 #########################################
 # Driver program
 if __name__ == "__main__":
-
 	exit_flag = 0
-
 	while exit_flag == 0:
 		print("\nWelcome to Number Converter ")
 		ch = input("To begin, choose (c)ontinue or (q)uit\n")
